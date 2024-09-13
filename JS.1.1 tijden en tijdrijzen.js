3b7fab6696459d5780294e18fc30d4490635448b
Isaiah2am <isaiahkoroma@hotmail.com>
2024-09-13 11:59:16 +0200

    Add files via upload

---
+++ JS.1.1 tijden en tijdrijzen.js
@@ -0,0 +1,13 @@
+const date1 = new Date ();    
+const date2 = new Date ("2024 7 12 10:39:5:10");
+const date3 = new Date ("12 Juli 1985,");
+const date4 = new Date ("23 September 1955");
+const date5 = new Date ("31 Augustus 2015");
+const date6 = new Date ("4 Januari 1885"); 
+
+console.log(date1);
+console.log(date2);
+console.log(date3);
+console.log(date4);
+console.log(date5);
+console.log(date6);
