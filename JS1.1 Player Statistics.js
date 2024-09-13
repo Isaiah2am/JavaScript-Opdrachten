856b3867f4a6c480aff81fefa4e28a497ccf09e3
Isaiah2am <isaiahkoroma@hotmail.com>
2024-09-13 12:44:12 +0200

    Add files via upload

---
+++ JS1.1 Player Statistics.js
@@ -0,0 +1,32 @@
+let name = "Isaiah Koroma";
+console.log("Name: " + name);
+
+let ethnicity = "Chineese/Nigerian";
+console.log("ethnicity: " + ethnicity);
+
+let ipadress = "10.120.3.241";
+console.log("Ip address: " + ipadress);
+
+let school = "Media College Amsterdam";
+console.log("School: "+ school);
+
+let shoes = "Jordan lows";
+console.log("Shoes: "+ shoes);
+
+let lives = "9";
+console.log("Lives: "+ lives);
+
+let phone = "Iphone 13";
+console.log("Phone: " + phone);
+
+let laptop = "Hp Gaming Laptop 16 by OMEN";
+console.log("Laptop: "+ laptop);
+
+let batterypower = "31%"
+console.log("Battery power: " + batterypower);
+
+let tierd = "yes";
+console.log("Tierd: "+ tierd);
+
+let Billionaire = "false";
+console.log("Billionaire: "+ false);
